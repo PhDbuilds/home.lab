2fc92a80437b528ad8d99623905a5ad565e370f7
astronuat <astronuat@spacevoyager.club>
2026-03-09 16:30:36 -0400

    Added missing ansible pubkey to test VMs

--- terraform/ansible-test-vms.tf
+++ terraform/ansible-test-vms.tf
@@ -81,7 +81,10 @@ resource "proxmox_virtual_environment_vm" "ansible_test" {
 
     user_account {
       username = "ansible"
-      keys     = [file("~/.ssh/id_ed25519.pub")]
+      keys = [
+        file("~/.ssh/id_ed25519.pub"),
+        file("~/.ssh/sirius_ansible.pub"),
+      ]
     }
   }
 
