4daf97e4065fd8e95d1075bb9fa5dbde0a24cfa0
astronuat <astronuat@spacevoyager.club>
2026-03-16 05:01:52 -0400

    built opnsense machine with tf successfully

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.14.7"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.98.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  api_token = var.proxmox_api_token
+  endpoint  = "https://192.168.1.180:8006"
+  insecure  = true
+}
+
+module "polaris" {
+  source = "./modules/OPNsense"
+}
+
---
+++ terraform/modules/OPNsense/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_version = "1.14.7"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.98.1"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "polaris" {
+  name          = "polaris"
+  node_name     = "lab"
+  on_boot       = true
+  started       = true
+  scsi_hardware = "virtio-scsi-single"
+  machine       = "q35"
+  bios          = "ovmf"
+  description   = "OPNsense"
+  tags          = ["firewall", "terraform"]
+
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "host"
+  }
+
+  cdrom {
+    file_id = "local:iso/OPNsense-26.1.2-dvd-amd64.iso"
+  }
+
+  operating_system {
+    type = "other"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  #  efi_disk {
+  #    datastore_id = var.datastore_id
+  #    type         = "4m"
+  #  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    iothread     = true
+    size         = 25
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "10.0.0.2/24"
+        gateway = "10.0.0.1"
+      }
+
+    }
+
+    user_account {
+      username = "astronuat"
+    }
+  }
+
+  # WAN
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  # LAN
+  network_device {
+    bridge = "vmbr1"
+  }
+
+  # TEST
+  network_device {
+    bridge = "vmbr2"
+  }
+
+  # SECLAB
+  network_device {
+    bridge = "vmbr3"
+  }
+
+}
+
